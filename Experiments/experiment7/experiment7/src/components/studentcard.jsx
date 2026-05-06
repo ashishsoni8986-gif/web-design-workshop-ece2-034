1be572a0f2addc331450b5fda64d18582338ac5c
ashish soni <ashishsoni8986@gmail.com>
2026-05-06 12:10:24 +0530

    commit

---
+++ Experiments/experiment7/experiment7/src/components/studentcard.jsx
@@ -0,0 +1,9 @@
+function studentcard({name, marks}) {
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>Marks: {marks}</p>
+        </div>
+    );
+}
+export default studentcard;
